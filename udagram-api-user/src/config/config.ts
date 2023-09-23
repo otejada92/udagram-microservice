c8142159570d90d0b27691298dd01b389fe1402c
Osvaldo Tejada <osvaldo.tl.92@gmail.com>
2023-09-23 09:27:29 -0400

    fix typo in config file

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -2,7 +2,7 @@ export const config = {
   'username': process.env.POSTGRESS_USERNAME,
   'password': process.env.POSTGRESS_PASSWORD,
   'database': process.env.POSTGRESS_DATABASE,
-  'host': process.env.POSTGRES_HOST,
+  'host': process.env.POSTGRESS_HOST,
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
