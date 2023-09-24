be78e237c93d5721d0266b91d5a1908de574c3c6
Osvaldo Tejada <osvaldo.tl.92@gmail.com>
2023-09-23 21:45:57 -0400

    update apiHost

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a22e5b5e842e44cf6b193baacc415f3f-1099082126.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
